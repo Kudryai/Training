d9423a13aa39c0239a26bb94104a06ed45f19475
Aleksey Kudryavtcev <st1gii@yahoo.com>
2022-06-16 10:56:50 +0300

    god job

--- Beginner+/Extend.py
+++ Beginner+/Extend.py
@@ -1,4 +1,8 @@
-from tkinter import PROJECTING
+list1 = [[1, 7, 8], [9, 7, 102], [102, 106, 105], [100, 99, 98, 103], [1, 2, 3]]
+total = 0
+counter = 0
+for mx in list1:
+    total += sum(mx)
+    counter += len(mx)
 
-
-n = PROJECTING
+print(total/counter)
