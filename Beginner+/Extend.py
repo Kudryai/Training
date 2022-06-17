9ef95fd20dc635e729f9e6b811f80263a9445e3c
Aleksey Kudryavtcev <st1gii@yahoo.com>
2022-06-17 09:31:12 +0300

    new trane

--- Beginner+/Extend.py
+++ Beginner+/Extend.py
@@ -1,8 +1,25 @@
-list1 = [[1, 7, 8], [9, 7, 102], [102, 106, 105], [100, 99, 98, 103], [1, 2, 3]]
-total = 0
-counter = 0
-for mx in list1:
-    total += sum(mx)
-    counter += len(mx)
-
-print(total/counter)
+n = int(input())
+def pasca(spisok):
+  list2 = [1]
+  for i in range(1,len(spisok)):
+    result = spisok[i]+spisok[i-1]
+    list2.append(result)
+  list2.append(1)
+  if len(list2)-1 == n:
+    return print(*list2)
+  else:
+    print(*list2)
+    pasca(list2)
+if n != 0 and n != 1 and n != 2:
+    n -= 1
+    print(*[1])
+    print(*[1,1])
+    pasca([1,1])
+else:
+    if n == 1:
+        print(*[1])
+    elif n == 2:
+        print(*[1])
+        print(*[1,1])
+    else:
+        print(*[1])
